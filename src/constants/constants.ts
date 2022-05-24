6467a675660c405abe315d8d6833b9a727c621ae
vladward <vladward13@gmail.com>
2022-05-24 17:25:00 +0300

    add paths

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,12 @@
 export const API_HOST: string = process.env.API_HOST as string;
 export const BASE_URL: string = process.env.BASE_URL as string;
+
+export const PATHS = {
+  home: '/',
+  character: '/character/:characterId',
+  characters: '/characters',
+  locations: '/locations',
+  location: '/locations/:locationId',
+  episodes: '/episodes',
+  episode: '/episodes/:episodesId',
+};
