848614b224cb684f4a88d91320ff3f5abb22a249
vladward <vladward13@gmail.com>
2022-05-24 17:24:02 +0300

    fix queries

--- src/graphql/queries/characters.gql.ts
+++ src/graphql/queries/characters.gql.ts
@@ -1,24 +1,24 @@
-import {gql} from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const GET_CHARACTERS = gql`
-    query characters($page: Int, $filter: FilterCharacter!) {
-        characters(page: $page, filter: $filter) {
-            info {
-                count
-                pages
-                next
-                prev
-            }
-            results {
-                id
-                name
-                status
-                species
-                type
-                gender
-                image
-                created
-            }
-        }
+  query characters($page: Int, $filter: FilterCharacter!) {
+    characters(page: $page, filter: $filter) {
+      info {
+        count
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        status
+        species
+        type
+        gender
+        image
+        created
+      }
     }
+  }
 `;
