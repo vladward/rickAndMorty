6f24055f3a4e5dc8cedd6466eb55de26c4a39133
vladward <vladward13@gmail.com>
2022-05-31 22:05:20 +0300

    fix Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,24 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import { HEADER_LINKS } from '../../constants/constants';
+import { Logo } from '../logo/Logo';
+import { useStyles } from './styles';
 
 export const Header: FC = () => {
+  const styles = useStyles();
   return (
-    <div style={{ height: '45px', margin: '0' }}>
-      <p>header</p>
+    <div className={styles.header}>
+      <nav className={styles.nav}>
+        <Logo />
+        <div className={styles.navLinks}>
+          {HEADER_LINKS.map((link, index) => (
+            <Link key={index} to={link.link} className={styles.linkButton}>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
     </div>
   );
 };
---
+++ src/components/header/styles.ts
@@ -0,0 +1,36 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+  header: {
+    height: '45px',
+    margin: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nav: {
+    width: '90%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  navLinks: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  linkButton: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '0 15px',
+    textDecoration: 'none',
+    color: 'black',
+    fontWeight: '500',
+    fontSize: '22px',
+    borderRadius: '5px',
+    textAlign: 'center',
+    '&:hover': {
+      boxShadow: '0px 10px 0px rgba(0, 50, 100, 0.2)',
+    },
+  },
+});
